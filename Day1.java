7bf3b4c76893a030c911e4130f5b4e1be8fc09f7
alexnkdev <alexnk@amazon.com>
2025-12-01 00:09:22 -0500

    2025 day1

---
+++ Day1.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+  public static void main(String[] args) throws Exception {
+    Scanner sc = new Scanner(new File("input.txt"));
+    int current = 50;
+    int ret= 0;
+    while (sc.hasNext()) {
+      String line = sc.nextLine();
+      if (line.startsWith("L")) {
+        int amount = Integer.parseInt(line.substring(1));
+        for (int k = 0; k < amount; k++) {
+          current--;
+          if (current < 0) {
+            current = 99;
+          }
+          if (current == 0) {
+            ret++;
+          }
+        }
+      } else if (line.startsWith("R")) {
+        int amount = Integer.parseInt(line.substring(1));
+        for (int k = 0; k < amount; k++) {
+          current++;
+          if (current == 100) {
+            current = 0;
+          
+          }
+          if (current == 0) {
+            ret++;
+          }
+        }
+      }
+    }
+    System.out.println(ret);
+    sc.close();
+  }
+}
