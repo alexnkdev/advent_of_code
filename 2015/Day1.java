ca0555abd57522bbbcb64ecbe32411deefcc7c7e
alexnkdev <alexnk@amazon.com>
2025-12-02 22:42:49 -0500

    2015 Day1

---
+++ 2015/Day1.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+
+  public static void main(String[] args) throws Exception {
+    Scanner sc = new Scanner(new File("input.txt"));
+    String line = sc.nextLine();
+    int floor = 0;
+    for (int i = 0; i < line.length(); i++) {
+      char c = line.charAt(i);
+      if (c == '(') {
+        floor++;
+      } else {
+        floor--;
+      }
+      if (floor == -1) {
+        System.out.println(i + 1);
+        break;
+      }
+    }
+    System.out.println(floor);
+    sc.close();
+  }
+}
