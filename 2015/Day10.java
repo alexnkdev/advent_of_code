d3f05eee2e80bd01da1d07072a5049120d62962e
alexnkdev <alexnk@amazon.com>
2025-12-02 23:31:25 -0500

    2015 Day 10

---
+++ 2015/Day10.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+
+  public static void main(String[] args) {
+    String line = "1113122113";
+    for (int step = 0; step < 50; step++) {
+      line = apply(line);
+      System.out.println(step + " done");
+    }
+    System.out.println(line.length());
+  }
+
+  public static String apply(String line) {
+    int i = 0;
+    StringBuilder ret = new StringBuilder();
+    while (i < line.length()) {
+      char c = line.charAt(i);
+      int cnt = 1;
+      while (i < line.length() - 1 && line.charAt(i + 1) == c) {
+        cnt++;
+        i++;
+      }
+      ret.append("" + cnt);
+      ret.append(c);
+      i++;
+    }
+    return ret.toString();
+  }
+
+}
