6d1852a3d0d625b8e2493d554bb335c91a013b08
alexnkdev <alexnk@amazon.com>
2025-12-07 00:21:30 -0500

    Day7 cleanup

--- 2025/Day7_2025.java
+++ 2025/Day7_2025.java
@@ -38,17 +38,9 @@ class Day7_2025 {
       }
     }
     long ret = 0;
-
-    for (int i = 0; i < n; i++) {
-      for (int j = 0; j < m; j++) {
-        System.out.print(cnt[i][j] + " ");
-
-        if (i == n - 1) {
-          ret+= cnt[i][j];
-        }
-      }
-      System.out.println();
-   }
+    for (int j = 0; j < m; j++) {
+      ret += cnt[n - 1][j];
+    }
     System.out.println(ret);
     sc.close();
   }
