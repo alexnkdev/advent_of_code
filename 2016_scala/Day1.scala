e0032a4f14d9b94ea1583034945a39aca449ad94
alexnkdev <alexnk@amazon.com>
2025-12-14 12:09:02 -0500

    Day1 more functional

--- 2016_scala/Day1.scala
+++ 2016_scala/Day1.scala
@@ -1,38 +1,45 @@
 import scala.io.Source
 
+def rotateLeft90(dir: (Int, Int)): (Int, Int) = (-dir._2, dir._1)
+def rotateRight90(dir: (Int, Int)): (Int, Int) = (dir._2, -dir._1)
+
+case class State(pos: (Int, Int), dir: (Int, Int))
+
 object Main {
   def main(args: Array[String]): Unit = {
     val source = Source.fromFile("Day1_input.txt")
-    val lines = source.getLines().toList
+    val instructions = source.getLines().next().split(", ").toList
     var direction: (Int, Int) = (-1, 0)
     var position: (Int, Int) = (0, 0)
+    
 
-    def rotateLeft90(dir: (Int, Int)): (Int, Int) = (-dir._2, dir._1)
-    def rotateRight90(dir: (Int, Int)): (Int, Int) = (dir._2, -dir._1)
+    val result = instructions.
+      foldLeft((State((0,0), (-1,0)), Set[(Int, Int)]((0, 0)), Option.empty[(Int, Int)])) {
+        case ((state, visited, firstRevisit), instr) =>
+          firstRevisit match {
+            case Some(_) => (state, visited, firstRevisit)
+            case None =>
+              val turn = instr.head
+              val steps = instr.tail.toInt
+              val newDir = if (turn == 'L') rotateLeft90(state.dir) else rotateRight90(state.dir)
 
-    var visited: Set[(Int, Int)] = Set()
-    // visited += position
-    var found = false;
+              val positions = (1 to steps).scanLeft(state.pos) {
+                case (pos, _) => (pos._1 + newDir._1, pos._2 + newDir._2)
+              }.tail
 
-    for (line <- lines) {
-      val steps = line.split(", ").map { instruction => 
-        val turn = instruction.charAt(0)
-        val stepLength = instruction.tail.toInt
+              val revisit = positions.find(visited.contains)
+              val newVisited = visited ++ positions
+              val newState = State(positions.last, newDir)
+              (newState, newVisited, revisit)
 
-        direction = if (turn == 'L') rotateLeft90(direction) else rotateRight90(direction);
-        
-        for (_ <- 1 to stepLength) {
-          position = (position._1 + direction._1, position._2 + direction._2)
-          if (!found && visited.contains(position)) {
-            println(Math.abs(position._1) + Math.abs(position._2));
-            found = true;
           }
-          visited += position
-        }
-
-
       }
+
+    result._3 match {
+      case Some(pos) => println(Math.abs(pos._1) + Math.abs(pos._2))
+      case None => println("No solution")
     }
+
     source.close()
 
   }
