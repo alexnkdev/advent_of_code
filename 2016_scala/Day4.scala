d8903e877ba94849a3523bb00f6541a3943d074f
alexnkdev <alexnk@amazon.com>
2025-12-20 22:32:44 -0500

    Day4 part 1

---
+++ 2016_scala/Day4.scala
@@ -0,0 +1,41 @@
+import scala.io.Source;
+
+object Main {
+
+  def main(args: Array[String]): Unit = {
+    solvePart1();
+    solvePart2();
+  }
+
+  def solvePart1(): Unit = {
+    val source = Source.fromFile("Day4_input.txt");
+    val ret = source.getLines()
+        .map {
+          line =>
+            val tokens = line.split("-");
+            val idToken = tokens.last;
+            val id = idToken.split("\\[")(0)
+            val checkSum = idToken.split("\\[").last.dropRight(1)
+
+            val freq = tokens.dropRight(1).mkString.foldLeft(Map.empty[Char, Int]) { (m, ch) => 
+              m.updated(ch, m.getOrElse(ch, 0) + 1)
+            }
+            
+            val top5 = freq.toSeq
+              .sortBy { case (k, v) => (-v, k) }
+              .take(5)
+              .map(_._1)
+
+
+            Array(top5.mkString, checkSum, id)
+            
+            }.map { case Array(checksum, expectedChecksum, id) => if (checksum == expectedChecksum) id.toInt else 0 }
+        .sum
+    println(ret);
+  }
+
+  def solvePart2(): Unit = {
+    println("Part 2");
+  }
+
+}
