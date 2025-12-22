5eead0c7afbfd6c73de876b9b08b81f723e5d30d
alexnkdev <alexnk@amazon.com>
2025-12-22 17:00:27 -0500

    Day7 part 2

--- 2016_scala/Day7.scala
+++ 2016_scala/Day7.scala
@@ -4,15 +4,39 @@ object Main {
 
   def main(args: Array[String]): Unit = {
     solvePart1()
+    solvePart2()
   }
 
   def solvePart1(): Unit = {
     val source = Source.fromFile("Day7_input.txt")
-    val ret = source.getLines().count {
-      isSupportsTLS(_)
-    }
+    val ret = source.getLines().count { isSupportsTLS(_) }
     println(ret);
     source.close()
+  }
+
+  def solvePart2(): Unit = {
+    val source = Source.fromFile("Day7_input.txt")
+    val ret = source.getLines().count { isSupportsSSL(_) }
+    println(ret)
+    source.close()
+  }
+
+  def isSupportsSSL(str: String): Boolean = {
+    val (_, superSet, hyperSet) =
+      str.sliding(3).foldLeft((false, Set.empty[String], Set.empty[String])) {
+        case ((_, a, b), w) if w(0) == '[' =>
+          (true, a, b)
+        case ((_, a, b), w) if w(0) == ']' =>
+          (false, a, b)
+        case ((in, a, b), w)
+          if w(0) == w(2) && w(0) != w(1) =>
+            if (in)
+              (in, a, b + s"${w(0)}${w(1)}${w(0)}")
+            else
+              (in, a + s"${w(1)}${w(0)}${w(1)}", b)
+        case (state, _) => state
+      }
+    superSet.exists(hyperSet.contains)
   }
 
   def isSupportsTLS(str: String): Boolean = {
