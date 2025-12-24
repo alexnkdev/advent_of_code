336443f301bb5c7d877a56369409f601faf15696
Aleksandr Nikiforov <alex@alexn.ee>
2025-12-24 06:07:33 +0200

    Day5 part 2

--- 2016_scala/Day5.scala
+++ 2016_scala/Day5.scala
@@ -4,10 +4,15 @@ object Main {
 
   def main(args: Array[String]): Unit = {
     solvePart1();
+    solvePart2();
   }
 
   def solvePart1(): Unit = {
     println(findPassword("ffykfhsq"))
+  }
+
+  def solvePart2(): Unit = {
+    println(findPasswordPart2("ffykfhsq"))
   }
 
   def findPassword(doorId: String): String = {
@@ -17,6 +22,22 @@ object Main {
       .filter(_.startsWith("00000"))
       .take(8)
       .map(_(5)).mkString
+  }
+
+  def findPasswordPart2(doorId: String): String = {
+    val passMap = Iterator
+      .from(0)
+      .map(n => md5(s"$doorId$n"))
+      .filter(_.startsWith("00000"))
+      .filter(hash => hash(5) >= '0' && hash(5) <= '7')
+      .scanLeft(Map.empty[Int, Char]) { (acc, hash) =>
+        val pos = hash(5).asDigit
+        if (!acc.contains(pos)) acc + (pos -> hash(6)) else acc
+      }
+      .dropWhile(_.size < 8)
+      .next()
+    (0 until 8).map(passMap).mkString
+
   }
   
   def md5(s: String): String = {
