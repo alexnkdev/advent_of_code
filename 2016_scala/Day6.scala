a3aa3afba4cdfe028fd7b41b402813a3bb1d89b7
Aleksandr Nikiforov <alex@alexn.ee>
2025-12-24 22:13:34 +0200

    Day6 part 2

--- 2016_scala/Day6.scala
+++ 2016_scala/Day6.scala
@@ -4,6 +4,7 @@ import scala.collection.immutable.TreeMap
 object Main {
   def main(args: Array[String]): Unit = {
     solvePart1()
+    solvePart2()
   }
 
   def solvePart1(): Unit = {
@@ -32,7 +33,28 @@ object Main {
   }
 
   def solvePart2(): Unit = {
-    
+    val source = Source.fromFile("Day6_input.txt")
+    val message = source
+      .getLines()
+      .foldLeft(TreeMap.empty[Int, List[Char]]) { (acc, line) =>
+        line.zipWithIndex.foldLeft(acc) {
+          case (m, (ch, i)) =>
+            m.updatedWith(i) {
+              case Some(chars) => Some(chars :+ ch)
+              case None => Some(List(ch))
+            }
+        } 
+      }
+      .map {
+        case (k, list) =>
+          list
+            .groupBy(identity)
+            .minBy { case (_, freq) => freq.size }
+            ._1
+
+      }.mkString
+    println(message)
+    source.close()
   }
 
 }
